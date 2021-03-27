950117b59398569d97905f8ce36e5baacf6ffc3e
Federico Augusto dos Santos <67246605+fede2s@users.noreply.github.com>
2021-03-27 03:12:37 -0300

    Add files via upload

---
+++ my script.js
@@ -0,0 +1,18 @@
+/* This script calculate's my age so i don't need to remember about changing it every year*/
+
+function calcularEdad(fecha) {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
+
+let years = calcularEdad('1996-09-06');
+
+document.getElementById("age").innerHTML = years;
