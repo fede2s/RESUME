200d3828fa1009ffada899a2238559737904a037
Fede <fede.2santos_96@hotmail.com>
2021-05-11 12:56:11 -0300

    change functions to arrow functions, change a few html onclick attributes for a few event listeners

--- my script.js
+++ my script.js
@@ -1,6 +1,7 @@
 /* This script calculates my age so i don't need to remember about changing it every year*/
 
-function calculateAge(date) {
+const birthday = '1996-09-06';
+const calculateAge = (date) => {
     var today = new Date();
     var birthday = new Date(date);
     var year = today.getFullYear() - birthday.getFullYear();
@@ -9,45 +10,53 @@ function calculateAge(date) {
     if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
         year--;
     }
-
     return year;
 }
 
-function sayhello(){
-    console.log("Hello, bro. Please hire me");
-}
-
-//Menu desplegable
-let menu = document.getElementById("menu");
-menuClosed = true;
-
-function deployMenu(){
-    menu.style.display="flex";
-    videoContainer.style.display='none';
-    menuClosed=false;
-}
-function closeMenu(){
-    menu.style.display="none";
-    menuClosed=true;
-}
-function buttonMenu(){
-    menuClosed ? deployMenu() : closeMenu();
-}
-
-const viewVideo = () => videoContainer.style.display="block"
-
-const sorry = () => alert("Sorry, i still working on that section");
-
-let birthday = '1996-09-06';
-
 let age = calculateAge(birthday);
-
 document.getElementById("age").innerHTML = age;
 
 
+
+/* Script to skip video */
 const videoContainer = document.getElementById("videoContainer");
+const viewVideo = () => {
+    videoContainer.style.display="block";
+    closeMenu();
+}
 document.getElementById('xButton').addEventListener('click', () => {
     videoContainer.style.display = "none";
 });
 
+//Menu desplegable
+const menu = document.getElementById("menu");
+let menuClosed = true;
+
+const deployMenu = () => {
+    menu.style.display="flex";
+    videoContainer.style.display='none';
+    menuClosed=false;
+}
+const closeMenu = () => {
+    menu.style.display="none";
+    menuClosed=true;
+}
+const buttonMenu = () => {
+    menuClosed ? deployMenu() : closeMenu();
+}
 document.getElementById("button-menu").addEventListener('click',buttonMenu);
+
+//Menu-Options
+//view video resume
+const videoButton = document.getElementById('viewVideo').addEventListener('click',viewVideo);
+
+//print my resume
+const printRESUME = () => {
+    closeMenu();
+    print();
+}
+const menuPrint = document.getElementById('printRESUME').addEventListener('click',printRESUME);
+
+
+
+
